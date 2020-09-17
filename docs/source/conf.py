8e7f66fea9731e2c89dbb9cfd0078dc01b556343
Bryan Jansens <bjansens@eoas.ubc.ca>
2020-09-17 14:36:24 -0700

    added theme to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,16 @@ release = '1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.autosectionlabel', \
+    'sphinx.ext.napoleon', 'sphinx_automodapi.automodapi', 'recommonmark', \
+        'sphinx_markdown_tables', 'sphinxcontrib.inlinesyntaxhighlight', \
+            'nbsphinx', 'sphinx.ext.mathjax']
+
+# use language set by highlight directive if no language is set by role
+inline_highlight_respect_highlight = False
+
+# use language set by highlight directive if no role is set
+inline_highlight_literals = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -39,7 +47,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,7 +55,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
